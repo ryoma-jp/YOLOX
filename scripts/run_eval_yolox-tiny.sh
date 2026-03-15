e3b92615842ad54b92b191ddca50ef898589f293
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2026-03-15 16:11:11 +0900

    Remove old data

--- scripts/run_eval_yolox-tiny.sh
+++ scripts/run_eval_yolox-tiny.sh
@@ -7,6 +7,7 @@ BASE_CMD="python -m tools.eval -n yolox-tiny -c weights/yolox_tiny.pth -b 64 -d 
 if [ "$EVAL_MODE" = "file" ]; then
 	EVAL_OUT_DIR="YOLOX_outputs/yolox_tiny/eval"
     mkdir -p "$EVAL_OUT_DIR"
+    rm -rf "$EVAL_OUT_DIR"/*
 	docker compose run --rm yolox \
 		$BASE_CMD --eval-out "$EVAL_OUT_DIR" --save-predictions
 else
