c437814b241d6cedb4f11f413d76a75d2fed26ca
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2026-03-15 16:11:42 +0900

    Running on the docker container

--- scripts/run_eval_yolox-s.sh
+++ scripts/run_eval_yolox-s.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-python -m tools.eval -n  yolox-s -c weights/yolox_s.pth -b 64 -d 1 --conf 0.001
+docker compose run --rm yolox \
+	python -m tools.eval -n yolox-s -c weights/yolox_s.pth -b 64 -d 1 --conf 0.001
--- scripts/run_train_yolox-tiny.sh
+++ scripts/run_train_yolox-tiny.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-python tools/train.py -f exps/custom/yolox_tiny.py -d 1 -b 64 --fp16 -o -c weights/yolox_tiny.pth
+docker compose run --rm yolox \
+	python tools/train.py -f exps/custom/yolox_tiny.py -d 1 -b 64 --fp16 -o -c weights/yolox_tiny.pth
